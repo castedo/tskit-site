fd43d5d1ec2c50d9c8df8c02cffb016badd66e2d
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-07-04 22:25:34 +0100

    WIP

--- _plugins/github_api.rb
+++ _plugins/github_api.rb
@@ -8,6 +8,7 @@ module Jekyll_Get_Github
     priority :highest
 
     def generate(site)
+      site.data['contributors'] = {}
       site.collections['software'].docs.each do |d|
         d.data['github'] = {}
         d.data['github']['releases'] = JSON.load(
@@ -19,7 +20,28 @@ module Jekyll_Get_Github
         d.data['github']['contributors'] = JSON.load(
           URI("https://api.github.com/repos/#{d['gh_org']}/#{d['name']}/contributors").open()
         )
+        d.data['github']['contributors'].each do |c|
+            if site.data['contributors'].key?(c['login'])
+                if d.data['github']['repo']['owner']['login'] == "tskit-dev"
+                    site.data['contributors'][c['login']]['contributions'] += c['contributions']
+                end
+            else
+                c['repos'] = []
+                site.data['contributors'][c['login']] = c
+                if d.data['github']['repo']['owner']['login'] != "tskit-dev"
+                    site.data['contributors'][c['login']]['contributions'] = 0
+                end
+            end
+            site.data['contributors'][c['login']]['repos'].append(d.data['github']['repo']['name'])
+        end
       end
+      site.data['contributors'] = site.data['contributors'].values.sort_by {|x| -x['contributions']}
+      site.data['contributors'] = site.data['contributors'].filter {|x| !x['login'].include?("[bot]") and x['login'] != "pyup-bot"}
+      puts site.data['contributors'][0].inspect
+      site.data['contributors'].each do |c|
+        c.merge!(JSON.load(URI("https://api.github.com/users/#{c['login']}").open()))
+      end
+      puts site.data['contributors'][0].inspect
     end
   end
 end
