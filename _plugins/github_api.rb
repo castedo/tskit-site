b393a0835217ce8dd2bc5cd0ff25ccbabc383daf
Ben Jeffery <ben.jeffery@gmail.com>
2022-07-06 14:05:45 +0100

    Tweak community

--- _plugins/github_api.rb
+++ _plugins/github_api.rb
@@ -27,18 +27,15 @@ module Jekyll_Get_Github
           HTTParty.get("https://api.github.com/repos/#{d['gh_org']}/#{d['name']}/contributors", headers: headers).body
         )
         d.data['github']['contributors'].each do |c|
-            if site.data['contributors'].key?(c['login'])
-                if d.data['github']['repo']['owner']['login'] == "tskit-dev"
+            if d.data['github']['repo']['owner']['login'] == "tskit-dev"
+                if site.data['contributors'].key?(c['login'])
                     site.data['contributors'][c['login']]['contributions'] += c['contributions']
+                else
+                    c['repos'] = []
+                    site.data['contributors'][c['login']] = c
                 end
-            else
-                c['repos'] = []
-                site.data['contributors'][c['login']] = c
-                if d.data['github']['repo']['owner']['login'] != "tskit-dev"
-                    site.data['contributors'][c['login']]['contributions'] = 0
-                end
+                site.data['contributors'][c['login']]['repos'].append(d.data['github']['repo']['name'])
             end
-            site.data['contributors'][c['login']]['repos'].append(d.data['github']['repo']['name'])
         end
       end
       site.data['contributors'] = site.data['contributors'].values.sort_by {|x| -x['contributions']}
