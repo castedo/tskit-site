c3fb24d709b98e8175c56ac5bea059b459db0bb8
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-08-08 17:09:38 +0100

    Implement filters

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -19,3 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 })
+
+window.switch_filter = (category) => {
+  $('a.active').removeClass('active')
+  $('[data-r-selector="'+category+'"]').addClass('active')
+  if (category === 'all') {
+    $('[data-r-type="paper"]').show()
+    $('[data-r-type="tutorial"]').show()
+    $('[data-r-type="video"]').show()
+  } else {
+    $('[data-r-type="paper"]').hide()
+    $('[data-r-type="tutorial"]').hide()
+    $('[data-r-type="video"]').hide()
+    $('[data-r-type="'+category+'"]').show()
+  }
+}
